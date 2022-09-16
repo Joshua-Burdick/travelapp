9440282749eaef6d05ff3727e19b4cba5bf2ed06
Joshua_Burdick <31868836+Joshua-Burdick@users.noreply.github.com>
2022-09-15 21:53:33 -0400

    Initial Packages and Setup
    Added Vuetify and VueLoader
    
    Set up /server/ initially for implementing backend.

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
+
+
+// Package application files and run dev server on port 1010
+const path = require('path');
+
+module.exports = {
+  outputDir: path.resolve(__dirname, '../server/public'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1010'
+      }
+    }
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json);
+
+const PORT = process.env.PORT || 1010;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+app.listen(PORT, () => {
+    console.log(`Listening on localhost:${PORT}`);
+})
