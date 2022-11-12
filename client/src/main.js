fc70b6e2e7aa6131ab6fa813056bf997e92e7d86
Yonava <yonavossandreae@gmail.com>
2022-11-12 11:37:57 -0500

    nokeys

--- client/src/main.js
+++ client/src/main.js
@@ -3,13 +3,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import * as VueGoogleMaps from 'vue2-google-maps'
-import Keys from './assets/keys.json'
 
 Vue.config.productionTip = false
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: Keys.Maps,
     libraries: "places"
   }
 });
