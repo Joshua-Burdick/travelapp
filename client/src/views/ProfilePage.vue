d23b0efd2d93d48a9a5aa420d6df6bfdcf9006a3
Yonava <yonavossandreae@gmail.com>
2022-12-01 16:37:59 -0500

    added profile decoration

--- client/src/views/ProfilePage.vue
+++ client/src/views/ProfilePage.vue
@@ -1,23 +1,44 @@
 <template>
   <div class="profile-container">
-    <v-card 
-      width="400"
-      class="card-container pb-2 px-2"
-      dark
-    >
-      <h1 class="text-h1">
-        {{ user }}
-      </h1>
-      <p>
-        {{ email }}
-      </p>
-      <v-btn 
-        @click="logout"
-        color="error"
+    <div class="title-card-container">
+      <div class="text-h5 font-weight-black white--text">
+        Your Profile:
+      </div>
+      <v-card 
+        width="400"
+        class="card-container pb-2 px-2"
+        dark
       >
-        Log Out
-      </v-btn>
-    </v-card>
+        <h1 class="text-h3 font-weight-black my-5">
+          {{ user }}
+        </h1>
+        <div class="text-h6 font-weight-black">
+          Registered Email Address:
+        </div>
+          <p>
+            {{ email }}
+          </p>
+        <v-card-actions class="card-actions">
+          <v-btn
+            @click="back"
+            text
+
+          >
+            <v-icon class="mr-2">mdi-arrow-left</v-icon>
+            Back
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn 
+            @click="logout"
+            color="error"
+            text
+          >
+            <v-icon class="mr-2">mdi-account-off</v-icon>
+            Log Out
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -41,7 +62,12 @@ export default {
   },
   methods: {
     logout() {
-      localStorage.removeItem('username');
+      localStorage.clear();
+      this.$router.push({
+        name: 'home'
+      });
+    },
+    back() {
       this.$router.push({
         name: 'home'
       });
@@ -56,13 +82,30 @@ export default {
   background-size: cover;
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
   height: 100vh;
   width: 100vw;
+}
+.title-card-container {
+  display: flex;
+  flex-direction: column;
+  align-items: left;
 }
 .card-container {
   border: 1px solid white; 
   border-radius: 10px; 
   background-color: rgba(255, 255, 255, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+.card-actions {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%
 }
 </style>
