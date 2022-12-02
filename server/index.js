1720ec5961cfe40cfeea0b30d779e9688724f31a
Yonava <yonavossandreae@gmail.com>
2022-12-01 21:01:09 -0500

    revitalized npm package contents

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_CONNECTION_URI);
