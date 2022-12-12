e431936a646db9ea08c715372596b27ad2a759e3
Matthew Fisher <matthew.fisher7@snhu.edu>
2022-12-12 14:05:53 -0500

    updated based on pr comments

--- client/src/views/WeatherPage.vue
+++ client/src/views/WeatherPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <div v-for="(weather, index) in weatherList" :key="weather.id">
-          <div v-if="index % 8  === 0">
+          <div v-if="index % 8  === 0"> <!--there are 8 3 hour periods in a day-->
               in {{( index * 3)}} hours, it will be
               {{weather.main.temp}} °F
           </div>
@@ -14,20 +14,20 @@
 export default {
   data() {
       return {
-          api_key: '8b81c25b4594c01f9e7d4caec4793f7a',
-          url_base: 'https://api.openweathermap.org/data/2.5/',
-          lat: "-34.0522",
-          lon: "118.2437",
+          lat: "",
+          lng: "",
           weather: {},
           weatherList: []      
        }
   },
   mounted() {
+      this.lat = Number(localStorage.getItem("lat")),
+      this.lng = Number(localStorage.getItem("lng")),
       this.fetchWeather()
   },
   methods: {
       fetchWeather() {
-          fetch(`${this.url_base}forecast?lat=${this.lat}&lon=${this.lon}&units=imperial&APPID=${this.api_key}`)
+          fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${this.lat}&lon=${this.lng}&units=imperial&APPID=8b81c25b4594c01f9e7d4caec4793f7a`)
               .then(res => res.json())
               .then(data => this.weatherList = data.list)
               .catch(err => console.log(err))
